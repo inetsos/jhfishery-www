0efc2a04dc6d958a529404c166231474d27590ab
inetsos <inetsos@naver.com>
2018-09-22 22:24:55 +0900

    error404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { WelcomeComponent } from './welcome/welcome.component';
+import { Error404Component } from './error404/error404.component';
 
 const routes: Routes = [
   { path: '',  component: WelcomeComponent },
+  { path: '**', component: Error404Component },
 ];
 
 @NgModule({
