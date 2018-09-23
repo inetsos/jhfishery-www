fd94c943c3e728e77fded252d9b05c64191891c0
inetsos <inetsos@naver.com>
2018-09-23 15:06:32 +0900

    login

--- src/app/util.service.ts
+++ src/app/util.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 import { ApiResponse } from './api-response';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,50 @@ export class UtilService {
     if(!environment.production) 
       console.error('An error occurred', error);
     return Promise.reject(error);
+  }
+
+  public updateFormErrors(form: FormGroup, formErrors: any, formErrorMessages: any) {
+    if (!form) { return; }
+
+    for (const field in formErrors) {
+      formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = formErrorMessages[field];
+        if(messages){
+          for (const key in control.errors) {
+            formErrors[field] += messages[key] + ' ';
+          }
+        }
+      }
+    }
+  }
+
+  public makeAllFormFieldsDirty(form: FormGroup) {
+    if (!form) { return; }
+
+    for (var field in form.controls) {
+      const control = form.get(field);
+      if(control) control.markAsDirty();
+    }
+  }
+
+  public makeFormDirtyAndUpdateErrors(form: FormGroup, formErrors: any, formErrorMessages: any) { //3
+    this.makeAllFormFieldsDirty(form);
+    this.updateFormErrors(form, formErrors, formErrorMessages);
+  }
+
+  public handleFormSubmitError(response: ApiResponse, form: FormGroup, formErrors: any): void { //4
+    if(response.errors){
+      for (const field in formErrors) {
+        const control = form.get(field);
+        if (response.errors[field] && response.errors[field].message) {
+          formErrors[field] += response.errors[field].message;
+        }
+      }
+      if(response.errors.unhandled){
+        response.message += response.errors.unhandled;
+      }
+    }
   }
 }
